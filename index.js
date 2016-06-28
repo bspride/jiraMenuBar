303d3da91e4daeef90793abc489de6057043a067
Michael Clarke <bspride11@gmail.com>
2016-06-28 19:13:14 -0400

    Move ipc code into ready block

--- index.js
+++ index.js
@@ -10,27 +10,25 @@ require('electron-debug')({showDevTools: true})
 mb.on('ready', () => {
   console.log('app is ready')
   // your app code here
-})
+  // IPC events
+  ipcMain.on('jira-connect', (event, args) => {
+    try {
+      jira = new Jira(args)
 
-// IPC events
-ipcMain.on('jira-connect', (event, args) => {
-  try {
-    jira = new Jira(args)
-
-    // Return some success message for now
-    if (jira) {
-      jira.getIssuesForCurrentUser((err, data) => {
-        if (err) {
-          event.sender.send('jira-connect-reply', err)
-        } else {
-          event.sender.send('jira-connect-reply', data)
-        }
-      })
-    } else {
-      event.sender.send('jira-connect-reply', 'Something went wrong, failed connection.')
+      // Return some success message for now
+      if (jira) {
+        jira.getIssuesForCurrentUser((err, data) => {
+          if (err) {
+            event.sender.send('jira-connect-reply', err)
+          } else {
+            event.sender.send('jira-connect-reply', data)
+          }
+        })
+      } else {
+        event.sender.send('jira-connect-reply', 'Something went wrong, failed connection.')
+      }
+    } catch (e) {
+      event.sender.send('jira-connect-reply', 'Failed to connect to Jira.')
     }
-  } catch (e) {
-    event.sender.send('jira-connect-reply', 'Failed to connect to Jira.')
-  }
+  })
 })
-
