48fff5f91c21e7affbdfa37a76ef8f7f3ff741f8
Michael Clarke <bspride11@gmail.com>
2016-06-28 21:25:49 -0400

    Add ability to specify protocol

--- src/client/models/settings.ts
+++ src/client/models/settings.ts
@@ -3,6 +3,7 @@ export class Settings {
     public basePath: string,
     public userName: string,
     public password: string,
-    public jql: string
+    public jql: string,
+    public protocol: string = 'https'
   ){}
 }
