d4e045f693ed1506771fd50fa3c6cdf0c8c2bb36
Michael Clarke <bspride11@gmail.com>
2016-07-03 14:33:55 -0400

    Add spinner html and css

--- src/client/components/loading.component.ts
+++ src/client/components/loading.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core'
 
 @Component({
     selector: 'loading-indicator',
-    templateUrl: '../templates/loading.template.html'
+    templateUrl: '../templates/loading.template.html',
+    styles: [require('../templates/css/loading.component.css')]
 })
 export class LoadingIndicator {}
 
