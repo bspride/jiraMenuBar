a04a97d962fe0cb07f90a1f85263365284f0144d
Robert Castillo <rcastillo@bh.com>
2016-07-03 20:07:23 -0500

    Changed comment component to use array of components instead of each component

--- src/client/components/comment.component.ts
+++ src/client/components/comment.component.ts
@@ -1,23 +1,13 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 @Component({
-  selector: 'my-comment',
+  selector: 'my-comments',
   templateUrl: '../templates/comment.template',
   styles: [require('../templates/css/comment.component.css')]
 })
 export class CommentComponent {
   @Input() commentObj: any
 
-  author: any
-  timeLog: Date
-
   ngOnInit () {
-    let self = this
-    self.author = self.commentObj.author
-
-    if (self.commentObj.updated > self.commentObj.created) {
-      self.timeLog = new Date(self.commentObj.updated)
-    } else {
-      self.timeLog = new Date(self.commentObj.created)
-    }
+    // TODO Set any class variables
   }
 }
